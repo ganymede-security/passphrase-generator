710859bdbb5e9e4aebb95765104d4fb596597cd0
christianm20358 <christianm20358@gmail.com>
2022-07-27 21:24:15 -0600

    Algorithm v1

---
+++ algorithm.go
@@ -0,0 +1,149 @@
+package passphrasegenerator
+
+import (
+	"log"
+)
+
+// The mask represents an array of bits representing a passphrase
+// No matter the order the items are arranged, in the combined
+// Length should equal the requested passphrase length
+type mask []int32
+
+// Phrase modifiers
+const (
+	PG_SPEC_CHAR int32 = 1 << iota
+	
+	PG_NUMBER
+
+	PG_SEPARATOR
+
+	PG_WORD
+	// Only the last word is given this value
+	PG_LAST_WORD
+	// TODO: Add a bitmask representing the lengths of each word
+	PG_WORD_LENGTH
+)
+
+// Words are an array of ints representing the lengths of each string 
+// that will be used in the phrase. 
+type words []int32
+
+// NewPhrase returns a shuffled list of words and a mask ready for
+// generating a passphrase
+func newPhrase(opts Options) (words, mask) {
+	ws := make([]int32, 0)
+	w, m := genMask(opts)
+	_, rm := addSeparators(w, m, opts)
+	for i:= 0; i < len(w)-1; i++ {
+		ws = append(ws, w[i] - 1)
+	}
+	ws = append(ws, w[len(w)-1])
+	return ws, rm
+}
+
+// Since separators will be added to each item except the last, their addition
+// does not count towards the total length. However, the lengths and number
+// of words must be known before separators can be added
+func addSeparators(w words, m mask, opts Options) (words, mask) {
+	last := w[len(w) - 1]
+	rt := make([]int32, 0)
+	rtm := make([]int32, 0)
+	sep := int32(len(opts.Separator))
+
+	for i:=0; i < len(w) - 1; i++ {
+		rt = append(rt, w[i] - sep)
+		rt = append(rt, sep)
+	}
+
+	rt = append(rt, last)
+
+	for i:=0; i < len(m); i++ {
+		switch m[i] {
+		case PG_NUMBER:
+			rtm = append(rtm, PG_NUMBER)
+
+		case PG_SPEC_CHAR:
+			rtm = append(rtm, PG_SPEC_CHAR)
+
+		case PG_WORD:
+			rtm = append(rtm, PG_WORD)
+			rtm = append(rtm, PG_SEPARATOR)
+
+		case PG_LAST_WORD:
+			rtm = append(rtm, PG_LAST_WORD)
+
+		}
+	}
+
+	return rt, rtm
+}
+
+// GetNumSizes returns an array containing the lengths of each word in the phrase.
+// Requires an input of the desired total phrase length, and desired numbers of
+// special characters, numbers, and whether the case should be changed
+func genWords(opts Options) (w words, err error) {
+	rl := int32(opts.PhraseLength - opts.SpecialChars - opts.Numbers)
+	words := make([]int32, 0)
+	var lw int32 = 0
+
+	for i := int32(0); i != rl; {
+		var rn int32 = 0
+		for rn < 1 {
+			rn, _ = getRandNum(len(opts.Separator)+2, opts.MaxWordLength - 1)
+		}
+		if rn+i > rl {
+			lw = rl - i
+			for lw == 0 || lw == 1 {
+				_, words = words[len(words)-1], words[:len(words)-1]
+				nlw := int32(0)
+				for _, v := range words {
+					if v > 0 {
+						nlw += v
+					} else {
+						nlw += (-v)
+					}
+				}
+				i = nlw
+				lw = rl - i
+			}
+			words = append(words, lw)
+			i += lw
+		} else {
+			words = append(words, rn)
+			i += rn
+		}
+	}
+	return words, nil
+}
+
+// genMask creates a phrase mask representing the different types of items
+// a phrase will need to generate.  
+func genMask(opts Options) (words, mask) {
+	n := opts.Numbers
+	sC := opts.SpecialChars
+
+	phraseMask := mask{}
+
+	for i := 0; i < n; i++ {
+		phraseMask = append(phraseMask, PG_NUMBER)
+	}
+
+	for i := 0; i < sC; i++ {
+		phraseMask = append(phraseMask, PG_SPEC_CHAR)
+	}
+
+	wds, err := genWords(opts)
+	if err != nil {
+		log.Print(err)
+	}
+
+	for i := 0; i < len(wds) - 1; i++ {
+		phraseMask = append(phraseMask, PG_WORD)
+	}
+	// Append last word
+	phraseMask = append(phraseMask, PG_LAST_WORD)
+
+	shuffled := CryptoShuffle(phraseMask)
+
+	return wds, shuffled
+}
---
+++ algorithm_test.go
@@ -0,0 +1,164 @@
+package passphrasegenerator
+
+import (
+	"log"
+	"testing"
+)
+
+var (
+	opts = Options{
+		MaxWordLength: 15,
+		PhraseLength:  32,
+		SpecialChars:  1,
+		Numbers:       1,
+		ChangeCase:    true,
+		Separator:     "-",
+	}
+)
+
+var expectedLength int32 = int32(opts.PhraseLength - opts.Numbers - opts.SpecialChars)
+
+func TestGenWords(t *testing.T) {
+	var wrds words
+	var err error
+	for i:=0; i<10000;i++ {
+		wrds, err = genWords(opts)
+		if err != nil {
+			t.Fail()
+			log.Print(err)
+		}
+		for i := 0; i < len(wrds); i++ {
+			if int(wrds[i]) > opts.MaxWordLength {
+				t.Fail()
+				log.Print(wrds)
+			}
+		}
+	}
+
+	
+	total := 0
+
+	for i := 0; i < len(wrds); i++ {
+		total += int(wrds[i])
+		if int(wrds[i]) > opts.MaxWordLength {
+			t.Fail()
+			log.Print(wrds)
+		}
+	}
+
+	if total != int(expectedLength) {
+		t.Fail()
+		log.Printf("Expected: %d\tGot: %d\t", expectedLength, total)
+	}
+
+	log.Print(wrds)
+}
+
+func TestGenMask(t *testing.T) {
+	_, mask := genMask(opts)
+	req := 0
+
+	for i := 0; i < len(mask); i++ {
+		switch mask[i] {
+		case PG_WORD:
+			continue
+		case PG_SPEC_CHAR:
+			continue
+		case PG_NUMBER:
+			continue
+		case PG_SEPARATOR:
+			t.Fail()
+		case PG_LAST_WORD:
+			req++
+		default:
+			t.Fail()
+		}
+	}
+
+	if req != 1 {
+		t.Fail()
+		log.Print(req)
+	}
+
+	log.Print(mask)
+}
+
+func TestAddSeparators(t *testing.T) {
+	w, m := genMask(opts)
+	req := 0
+	wl := 0
+	sl := 0
+
+	words, mask := addSeparators(w, m, opts)
+
+	for i := 0; i < len(mask); i++ {
+		switch mask[i] {
+		case PG_WORD:
+			wl++
+			continue
+		case PG_SPEC_CHAR:
+			continue
+		case PG_NUMBER:
+			continue
+		case PG_SEPARATOR:
+			sl++
+		case PG_LAST_WORD:
+			req++
+		default:
+			log.Printf("Failure: %d\n", i)
+			t.Fail()
+		}
+	}
+
+	if sl != wl {
+		t.Fail()
+		log.Printf("Expected: %d\tGot: %d\t", wl, sl)
+	}
+
+	if req != 1 {
+		t.Fail()
+		log.Printf("Expected: %d\tGot: %d\t", 1, req)
+	}
+
+	log.Print(words)
+	log.Print(mask)
+}
+
+func TestNewPhrase(t *testing.T) {
+	rw, rm := newPhrase(opts)
+	mods := opts.Numbers + opts.SpecialChars
+	newest := make([]int32, 0)
+
+	total := int32(0)
+
+	for _, v := range rw {
+		total += v
+		newest = append(newest, v - 1)
+	}
+
+	for i:=0; i<10000;i++ {
+		list, _ := newPhrase(opts)
+
+		for _, v := range list {
+			if v > int32(opts.MaxWordLength) {
+				t.Fail()
+				log.Print(v)
+			}
+		}
+	}
+
+	flags := 0
+
+	for i:=0; i < len(rm);i++ {
+			if rm[i] == PG_SEPARATOR {
+				flags++
+			}
+	}
+
+	log.Print(flags)
+
+	log.Print(total + int32(flags) + int32(mods))
+
+	log.Print(rw)
+	log.Print(rm)
+}
